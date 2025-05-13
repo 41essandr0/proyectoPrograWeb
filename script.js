ea2416212349c4b48b848e5ab7d46006df4f2566
O23224 <O23224@lab.aca>
2025-05-12 21:03:05 -0500

    import inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let juegos = [
+    
+    { id: 1, nombre: "FIFA 24", precio: 59.99, plataforma: "PS5" },
+    { id: 2, nombre: "Elden Ring", precio: 49.99, plataforma: "Windows" },
+    { id: 3, nombre: "Zelda Tears", precio: 69.99, plataforma: "Switch" },
+    { id: 4, nombre: "Cyberpunk", precio: 29.99, plataforma: "Windows" },
+]
+
+function mostrarJuegos() {
+    
+}
